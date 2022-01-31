c4a59bedf294c42207538b63b409f25f9125a9c7
Jaswal-Tania <77944972+Jaswal-Tania@users.noreply.github.com>
2022-01-30 20:31:56 -0800

    Update bus.cpp

--- bus.cpp
+++ bus.cpp
@@ -2,77 +2,104 @@
 
 
 template<typename ItemType>
-Bus<ItemType>::Bus() {}
-
-template<typename ItemType>
-Bus<ItemType>::~Bus() {}
-
-template<typename ItemType>
-vector<People> Getpeople() { 
-    return People;
+Bus<ItemType>::Bus() : Count(0){
 }
 
 template<typename ItemType>
-void Bus<ItemType>::Setpeople(vector<People> val) { people = val; }
+void Bus<ItemType>::load(const ItemType&){
 
-template<typename ItemType>
-void Bus<ItemType>::loadPeople(People people){
-    this->people.push_back(people);
-    this->sortPeople();
 }
 
 template<typename ItemType>
-void Bus<ItemType>::unloadPeople(People people){
-    bool found = false;
+void Bus<ItemType>::unload(const ItemType&){
 
-    for (int i = 0; i < this->people.size(); i++){
-        if (this->people.at(i).getName() == people.getName()){
-        this->people.erase(this->people.begin() + i);
-        }
-    }
-
-    if (found == true){
-        cout << "Person has got off the bus!" << endl;
-        this->sortPeople();
-    }
-
-    else{
-        cout << "Person is not on the bus!" << endl;
-    }
 }
 
 template<typename ItemType>
-void Bus<ItemType>::emptyAllPeople(){
-    this->people.clear();
+void Bus<ItemType>::emptyAllItems(){
+    return count = 0;
 }
 
 template<typename ItemType>
-ItemType Bus<ItemType>::peopleCount(){
-    return this->people.size();
+bool Bus<ItemType>::isEmpty(){
+    return count == 0;
 }
+
 
 template<typename ItemType>
-void Bus<ItemType>::sortPeople(){
-    People temp;
-
-    for (int i = 0; i < this->people.size(); i++){
-        for (int j = 0; j < this->people.size() - i - 1; j++){
-            if (this->people.at(j).getHeight() < this->people.at(j + 1).getHeight()){
-                temp = this->people.at(j);
-                this->people.at(j) = this->people.at(j + 1);
-                this->people.at(j + 1) = temp;
-            }
-        }
-    }
+int Bus<ItemType>::itemCount(){
+    return Count;
 }
 
-template<typename ItemType>
-ItemType Bus<ItemType>::isEmpty(){
-    if (this->people.size() == 0){
-        return true;
-    }
+// template<typename ItemType>
+// Bus<ItemType>::~Bus() {}
 
-    else{
-        return false;
-    }
-}
+// template<typename ItemType>
+// vector<People> GetPeople() { 
+//     return People;
+// }
+
+// template<typename ItemType>
+// void Bus<ItemType>::Setpeople(vector<People> val) { people = val; }
+
+// template<typename ItemType>
+// void Bus<ItemType>::loadPeople(People people){
+//     this->people.push_back(people);
+//     this->sortPeople();
+// }
+
+// template<typename ItemType>
+// void Bus<ItemType>::unloadPeople(People people){
+//     bool found = false;
+
+//     for (int i = 0; i < this->people.size(); i++){
+//         if (this->people.at(i).getName() == people.getName()){
+//         this->people.erase(this->people.begin() + i);
+//         }
+//     }
+
+//     if (found == true){
+//         cout << "Person has got off the bus!" << endl;
+//         this->sortPeople();
+//     }
+
+//     else{
+//         cout << "Person is not on the bus!" << endl;
+//     }
+// }
+
+// template<typename ItemType>
+// void Bus<ItemType>::emptyAllPeople(){
+//     this->people.clear();
+// }
+
+// template<typename ItemType>
+// ItemType Bus<ItemType>::peopleCount(){
+//     return this->people.size();
+// }
+
+// template<typename ItemType>
+// void Bus<ItemType>::sortPeople(){
+//     People temp;
+
+//     for (int i = 0; i < this->people.size(); i++){
+//         for (int j = 0; j < this->people.size() - i - 1; j++){
+//             if (this->people.at(j).getHeight() < this->people.at(j + 1).getHeight()){
+//                 temp = this->people.at(j);
+//                 this->people.at(j) = this->people.at(j + 1);
+//                 this->people.at(j + 1) = temp;
+//             }
+//         }
+//     }
+// }
+
+// template<typename ItemType>
+// ItemType Bus<ItemType>::isEmpty(){
+//     if (this->people.size() == 0){
+//         return true;
+//     }
+
+//     else{
+//         return false;
+//     }
+// }
